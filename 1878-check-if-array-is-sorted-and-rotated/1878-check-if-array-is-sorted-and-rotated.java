da7be7f97a3844d706b4248032ee49bd06968e2a
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-06-04 11:55:38 +0530

    Time: 0 ms (100%), Space: 41.5 MB (29.07%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean check(int[] nums) {
+        //Check if nums is sorted or not if yes then return true
         if(isSorted(nums))
         {
             return true;
         }
         int count = 0;
+        //Check from which index you have to rotate so that array become sorted if possible
          for(int i = 0; i < nums.length-1; i++)
          {
             if(nums[i+1]<nums[i])
             {
-                count=i+1;
+                count=i+1;//From here nums is not sorted
                 break;
             }
          }
+         //Create a new array and rotate the nums from index you found earlier
          int[] answer = new int[nums.length];
          for(int i = 0; i < nums.length; i++)
          {
             answer[i] = nums[(i+count)%nums.length];
          }
+         //Now check if answer is sorted or not
          return isSorted(answer);
     }
 
