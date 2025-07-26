fad740259612858334dd28b06aa5787e62f8c45a
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-07-26 14:16:35 +0530

    Time: 2 ms (73.82%), Space: 53.1 MB (59.54%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,21 @@
+import java.util.Map;
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+
+
+        //2nd Approach
+        // Arrays.sort(nums);
+        // return nums[nums.length/2];
+    }
+}
