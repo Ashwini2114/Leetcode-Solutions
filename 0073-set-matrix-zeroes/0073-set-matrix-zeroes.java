a439ed2ae10a346fd5da50975dcbf7e955e966b5
Ashwini Kumar <132124527+Ashwini2114@users.noreply.github.com>
2025-08-23 11:56:49 +0530

    Time: 1 ms (72.07%), Space: 45.4 MB (94.33%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -0,0 +1,76 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        //Time complexity : 0(2*n*m)
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int col0 = 1;
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(matrix[i][j] == 0)
+                {
+                    matrix[i][0] = 0; //Mark the i-th row 
+                    if(j != 0){
+                    matrix[0][j] = 0;} //Mark the j-th col 
+                    else{
+                        col0 = 0;}
+                }
+            }
+        }
+
+        for(int i = 1; i < m; i++)
+        {
+            for(int j = 1; j < n; j++)
+            {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(matrix[0][0] == 0)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if(col0 == 0)
+        {
+            for(int i = 0; i < m; i++)
+            {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+        //Time complexity:- 0(n*m)*(n+m) + 0(n*m)
+        // int m = matrix.length;
+        // int n = matrix[0].length;
+        // int[] col = new int[n];
+        // int[] row = new int[m];
+        // for(int i = 0; i < m; i++)
+        // {
+        //     for(int j = 0; j < n; j++)
+        //     {
+        //         if(matrix[i][j] == 0)
+        //         {
+        //             row[i] = 1;
+        //             col[j] = 1;
+        //         }
+        //     }
+        // }
+
+        // for(int i = 0; i < m; i++)
+        // {
+        //     for(int j = 0; j < n; j++)
+        //     {
+        //         if(row[i] == 1 || col[j] == 1)
+        //         {
+        //             matrix[i][j] = 0;
+        //         }
+        //     }
+        // }
